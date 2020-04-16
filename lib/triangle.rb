9308fbc1cd4e5a131ca244b1c1c5b9d4b15a9544
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-16 20:29:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,29 +1,91 @@
-class Triangle
-  def initialize(side_1,side_2,side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
-  end
+require 'set'
 
-  def kind()
-    if (@side_1 = 0) || (@side_2 = 0) || (@side_3 = 0)
-      raise TriangleError
-    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
-      raise TriangleError
-    else
-      if (@side_1 == @side_2) && (@side_2 == @side_3)
-        :equilateral
-      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
-        :isosceles
-      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
-        :scalene
-      end
+class Types
+
+    def self.isosceles
+      :isosceles
+    end
+
+    def self.scalene
+      :scalene
+    end
+
+    def self.equilateral
+      :equilateral
     end
 
   end
 
+  class TriangleError < StandardError
+    def message
+      "This triangle is not valid"
+    end
+  end
+
 end
 
-class TriangleError < StandardError
-  # triangle error code
-end
+class Triangle
+
+  attr_accessor :sides
+
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3]
+    @kind = set_kind
+  end
+
+  def check
+    valid?
+  end
+
+  def kind
+    raise TriangleError.new if !valid?
+
+    @kind
+  end
+
+  def valid?
+    return false if @sides.any?{|side| side <= 0}
+    case @kind
+    when Types.scalene
+      return longest < @sides.reject{|side| side == longest}.reduce{|cum, cur| cum + cur}
+    when Types.isosceles
+      larger = @sides.reject{|side| side == smallest}
+      puts larger.length
+      puts smallest * 2
+      return larger.length == 2 || smallest * 2 > larger[0]
+    when Types.equilateral
+      return true
+    else
+      puts "This should never be run"
+    end
+  end
+
+  def set_kind
+    case unique_sides
+    when 1
+      return Types.equilateral
+    when 2
+      return Types.isosceles
+    when 3
+      return Types.scalene
+    else
+      raise TraingleError
+    end
+
+  end
+
+  def longest
+    largest = 0
+    @sides.each{|side| largest = side if side > largest}
+    largest
+  end
+
+  def smallest
+    smallest = Float::INFINITY
+    @sides.each{|side| smallest = side if side < smallest}
+    smallest
+  end
+
+  def unique_sides
+    Set.new(@sides).size
+  end
